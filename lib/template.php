3949dd1228b2365c2e3774ac5c9e9c63f4bb4b09
Daniel Pekarek <pekarekdaniel@gmail.com>
2015-10-26 19:24:29 +0100

    footer updated

--- lib/template.php
+++ lib/template.php
@@ -78,20 +78,8 @@ $navbar
 		</div>
 
 		<div id="footer">
-			<div class="copy">&copy; <a href="http://hackyourlife.tk">hackyourlife</a></div>
+			<div class="copy">&copy; <a href="https://github.com/hackyourlife/evoc">hackyourlife</a></div>
 		</div>
-		<!-- Piwik --> 
-		<script type="text/javascript">
-		var pkBaseURL = (("https:" == document.location.protocol) ? "https://stat.patrickbuchner.tk/" : "http://stat.patrickbuchner.tk/");
-		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-		</script><script type="text/javascript">
-		try {
-		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
-		piwikTracker.trackPageView();
-		piwikTracker.enableLinkTracking();
-		} catch(err) {}
-		</script><noscript><p><img src="http://stat.patrickbuchner.tk/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
-		<!-- End Piwik Tracking Code -->
 	</body>
 </html>
 EOT
